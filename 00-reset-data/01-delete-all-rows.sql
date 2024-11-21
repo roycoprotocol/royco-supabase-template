1cf340f0d10bb4f74c08b77a83fac667ec0c9e72
githubotoro <yupuday.git@gmail.com>
2024-11-21 11:42:59 -0800

    feat(update): reset command

--- 00-reset-data/01-delete-all-rows.sql
+++ 00-reset-data/01-delete-all-rows.sql
@@ -1,6 +1,26 @@
-DELETE FROM public.raw_markets;
-DELETE FROM public.raw_offers;
-DELETE FROM public.raw_activities;
-DELETE FROM public.raw_account_balances_recipe;
-DELETE FROM public.raw_account_balances_vault;
-DELETE FROM public.raw_positions_recipe;
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_markets') THEN
+        DELETE FROM public.raw_markets;
+    END IF;
+    
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_offers') THEN
+        DELETE FROM public.raw_offers;
+    END IF;
+    
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_activities') THEN
+        DELETE FROM public.raw_activities;
+    END IF;
+    
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_account_balances_recipe') THEN
+        DELETE FROM public.raw_account_balances_recipe;
+    END IF;
+    
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_account_balances_vault') THEN
+        DELETE FROM public.raw_account_balances_vault;
+    END IF;
+    
+    IF EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'raw_positions_recipe') THEN
+        DELETE FROM public.raw_positions_recipe;
+    END IF;
+END $$;
